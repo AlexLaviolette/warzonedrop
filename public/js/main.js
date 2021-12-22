524557e630a99662f45d8543a2a01a585cacd208
Alex Laviolette <alex.laviolette92@gmail.com>
2021-12-21 23:02:04 -0500

    Hide menu when click outside

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,10 @@
 $(window).on("load", function() {
+  // Menu toggling/hiding
   $(".menu").click(function() {
     $(".dropdown-menu").toggle();
+  });
+
+  $(".click").click(function() {
+    $(".dropdown-menu").hide();
   });
 });
